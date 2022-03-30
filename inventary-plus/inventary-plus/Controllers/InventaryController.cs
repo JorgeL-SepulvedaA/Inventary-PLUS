7aff34d3ce2827422f8ec3c3d908b2541d59f27b
JaimeTerrero <jaimeterrero28@gmail.com>
2022-03-30 14:42:05 -0400

    Loading Interface

---
+++ inventary-plus/inventary-plus/Controllers/InventaryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace inventary_plus.Controllers
+{
+    public class InventaryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
