f55799643c7178e44cb05f1f52f3ccf20e13dfe1
JaimeTerrero <jaimeterrero28@gmail.com>
2022-04-29 11:46:50 -0400

    front-update

---
+++ Inventary-App/Inventary-App/Controllers/InventaryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inventary_App.Controllers
+{
+    public class InventaryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ Inventary-App/Inventary-App/wwwroot/js/index.js
@@ -0,0 +1,23 @@
+﻿let form = document.querySelector('#form');
+let alertField = document.querySelector('.alert-field');
+
+document.addEventListener('DOMContentLoaded', () => {
+    form.addEventListener('submit', dataValidation);
+});
+
+function dataValidation(e) {
+    e.preventDefault();
+
+    let userName = document.querySelector('#userName').value;
+    let userEmail = document.querySelector('#email').value;
+    let userPassword = document.querySelector('#password').value;
+
+    if (userName == '' || userEmail == '' || userPassword == '') {
+        swal("Error!", "Todos los campos son obligatorios", "error");
+
+        return;
+    }
+
+
+    swal("Bien hecho", "Usuario creado!", "success");
+}
